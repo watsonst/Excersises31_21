6859ac2cb48422ef5ca087ff50d9f8bbe1b9153d
Stephanie <me@stephaniemariewatson.com>
2021-08-23 15:38:26 -0400

    Initial commit for Exersices 31 and 32

---
+++ Exercise_32/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Exercise_32
+{
+    class program
+    { 
+    static void Main(string[] args)
+    {
+        IndexOfUserInput();
+    }
+
+    public static void IndexOfUserInput()
+    {
+        string answer = string.Empty;
+
+        do
+        {
+            try 
+            { 
+
+            string[] givenNumbersArray = { "2", "8", "0", "24", "51" };
+
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            int userIndex = Convert.ToInt32(userInput);
+
+                if (userIndex.ToString() != "2" && userIndex.ToString() != "8" && userIndex.ToString() != "0" && userIndex.ToString() != "24" && userIndex.ToString() != "51")
+                    {
+                    Console.WriteLine($"That value cannot be found in the array");
+                    }
+
+                else
+                    {
+                     int arrayElement = Array.IndexOf(givenNumbersArray, userInput);
+                     Console.WriteLine(value: $"The value {userInput} can be found at index {arrayElement}");
+                    }
+            }
+            catch (FormatException ex)
+            {
+                    Console.WriteLine($"That value cannot be found in the array");
+            }
+    
+
+            Console.Write("Would you like to continue? (y/n): ");
+            answer = Console.ReadLine().ToLower();
+
+            if (answer == "n")
+            {
+                Console.WriteLine("GoodBye!");
+                break;
+            }
+
+
+        } while (answer == "y");
+
+    }
+}
+}
---
+++ Exercises31_32/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exercises31_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueOfUserIndex1();
+        }
+
+
+    public static void ValueOfUserIndex1()
+    {
+        string answer = string.Empty;
+
+        do
+        {
+            int[] givenNumbersArray = { 2, 8, 0, 24, 51 };
+
+            Console.Write("Enter an index of the array: ");
+            string userInput = Console.ReadLine();
+            int userIndex = Convert.ToInt32(userInput);
+
+            try
+            {
+                int arrayElement = givenNumbersArray[userIndex];
+
+                Console.WriteLine(value: $"The value at index {userInput} is {arrayElement}");
+            }
+
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"That is not a valid index.");
+            }
+
+            Console.Write("Would you like to continue? (y/n): ");
+            answer = Console.ReadLine().ToLower();
+
+            if (answer == "n")
+            {
+                Console.WriteLine("GoodBye!");
+                break;
+            }
+
+
+        } while (answer == "y");
+
+    }
+}
+ }
+    
